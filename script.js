3374bff5d519933ada8948e1ff3089d53e16efd0
ptrkns <keneseipeterr@gmail.com>
2024-04-09 11:11:19 +0200

    Arrows, and click events

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ const path = './assets/images.json';
 let currentPicture = 0;
 let imageArray = [];
 
-function loadPicture(index) {
+function loadMainPicture(index) {
     $('#main-image').attr('src', imageArray[index].image);
     $('#title').text(imageArray[index].title);
     $('#description').text(imageArray[index].description);
+    updateThumbnails(index);
+};
+
+function setThumbnailClick(name, index) {
+    $(name).on('click', () => {
+        currentPicture = index;
+        loadMainPicture(index);
+    });
+};
+
+function setThumbnail(name, index) {
+    $(name).attr('src', imageArray[index].image);
+    setThumbnailClick(name, index);
+};
+
+function updateThumbnails(index) {
+    setThumbnail('#th-image-2', index);
+    if( index === 0 ) {
+        setThumbnail('#th-image-1', imageArray.length - 1);
+        setThumbnail('#th-image-3', index + 1);
+    }
+    else if ( index > 0 && index < imageArray.length - 1 ) {
+        setThumbnail('#th-image-1', index - 1);
+        setThumbnail('#th-image-3', index + 1);
+    }
+    else if ( index === imageArray.length - 1 ) {
+        setThumbnail('#th-image-1', index - 1);
+        setThumbnail('#th-image-3', 0);
+    }
 };
 
 function imageArrayInit() {
@@ -14,49 +43,30 @@ function imageArrayInit() {
     .then((response) => { return response.json(); })
     .then((data) => {
         imageArray = data;
-
-        loadPicture(currentPicture);
-
-        $('#th-image-1').attr('src', imageArray[0].image);
-        $('#th-image-2').attr('src', imageArray[1].image);
-        $('#th-image-3').attr('src', imageArray[2].image);
-        $('#th-image-4').attr('src', imageArray[3].image);
-        $('#th-image-5').attr('src', imageArray[4].image);
-
+        loadMainPicture(currentPicture);
+        updateThumbnails(currentPicture);
     })
     .catch((error) => { console.error("There was a problem with the fetch operation: ", error); });
 };
 imageArrayInit();
 
-// Left Arrow
-$('#al').on('click', () => {
-    if(currentPicture > 0) {currentPicture--;}
-    else if(currentPicture === 0) {currentPicture = imageArray.length-1;}
-    for(let i = 0; i < imageArray.length; i++) {
-        if(currentPicture === i){
-            $('#main-image').attr('src', imageArray[currentPicture].image);
-            $('#title').text(imageArray[currentPicture].title);
-            $('#description').text(imageArray[currentPicture].description);
-        }
-    }
-});
+function setArrowClick(name) {
+    $(name).on('click', () => {
+        if (name === '#al' && currentPicture > 0) {currentPicture--;}
+        else if (name === '#al' && currentPicture === 0) {currentPicture = imageArray.length - 1;}
 
-// Right Arrow
-$('#ar').on('click', () => {
-    if(currentPicture < imageArray.length) {currentPicture++;}
-    else if(currentPicture === imageArray.length) {currentPicture = 0;}
-    for(let i = 0; i < imageArray.length; i++) {
-        if(currentPicture === i){
-            $('#main-image').attr('src', imageArray[currentPicture].image);
-            $('#title').text(imageArray[currentPicture].title);
-            $('#description').text(imageArray[currentPicture].description);
-        }
-    }
-});
+        if (name === '#ar' && currentPicture < imageArray.length - 1) {currentPicture++;}
+        else if (name === '#ar' && currentPicture === imageArray.length - 1) {currentPicture = 0;}
 
-// Thumbnails
-$('#th-image-1').on('click', () => { currentPicture = 0; loadPicture(currentPicture); });
-$('#th-image-2').on('click', () => { currentPicture = 1; loadPicture(currentPicture); });
-$('#th-image-3').on('click', () => { currentPicture = 2; loadPicture(currentPicture); });
-$('#th-image-4').on('click', () => { currentPicture = 3; loadPicture(currentPicture); });
-$('#th-image-5').on('click', () => { currentPicture = 4; loadPicture(currentPicture); });
+        for (let i = 0; i < imageArray.length; i++) {
+            if (currentPicture === i) {
+                $('#main-image').attr('src', imageArray[currentPicture].image);
+                $('#title').text(imageArray[currentPicture].title);
+                $('#description').text(imageArray[currentPicture].description);
+                updateThumbnails(currentPicture);
+            }
+        }
+    });
+};
+setArrowClick('#al');
+setArrowClick('#ar');
